7ee54d2c3a8fa004aedd4b6b05ee79d4662ff495
josephrayrusk <35713802+josephrayrusk@users.noreply.github.com>
2018-02-22 17:47:39 -0800

    Days Between Dates

---
+++ Days Between Dates/Days Between Dates/Days Between Dates.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Days Between Dates.aspx.cs" Inherits="Days_Between_Dates.Days_Between_Dates" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            How may days have elapsed?<br />
+            <br />
+            Choose a Date:<asp:Calendar ID="Calendar1" runat="server"></asp:Calendar>
+            <br />
+            Choose a Second Date<asp:Calendar ID="Calendar2" runat="server"></asp:Calendar>
+            <br />
+        </div>
+        <asp:Button ID="daysButton" runat="server" OnClick="Button1_Click" Text="Days Elsapsed" />
+        <br />
+        <br />
+        <asp:Label ID="elapsedDays" runat="server"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Days Between Dates/Days Between Dates/Days Between Dates.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Days_Between_Dates
+{
+    public partial class Days_Between_Dates : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Calendar1.SelectedDate > Calendar2.SelectedDate)
+            {
+                elapsedDays.Text = Calendar1.SelectedDate.Subtract(Calendar2.SelectedDate).TotalDays.ToString() + " Days";
+            }
+            else if (Calendar2.SelectedDate > Calendar1.SelectedDate)
+            {
+                elapsedDays.Text = Calendar2.SelectedDate.Subtract(Calendar1.SelectedDate).TotalDays.ToString() + " Days";
+            }
+            else elapsedDays.Text = "0 Days. Who are you trying to fool?";
+        }
+    }
+}
